37b65fe178e4c4608864e803c2fba4abdd353361
Darknught <hassino66@yahoo.com>
2024-02-20 18:03:22 +0000

    code

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """init"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 import os
 
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
 if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     storage = FileStorage()
