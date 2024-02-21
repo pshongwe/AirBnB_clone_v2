a8be2c45952e024f499b5fd1074b845c47df3263
Darknught <hassino66@yahoo.com>
2024-02-21 11:13:35 +0000

    code

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ class User """
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """ class representation of User object """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
