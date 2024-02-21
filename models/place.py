5a3371271efe87b3ac113bf0e30dbb94c85009b0
Darknught <hassino66@yahoo.com>
2024-02-21 13:32:39 +0000

    code

--- models/place.py
+++ models/place.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Module that defines class Place"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Colum, String, Integer, Float, ForeignKey
+from models.user import User
+from models.city import City
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """Place class that inherits from BaseModel"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.city_id = kwargs.get('city_id', "")
-        self.user_id = kwargs.get('user_id', "")
-        self.name = kwargs.get('name', "")
-        self.description = kwargs.get('description', "")
-        self.number_rooms = kwargs.get('number_rooms', 0)
-        self. number_bathrooms = kwargs.get('number_bathrooms', 0)
-        self.max_guest = kwargs.get('max_guest', 0)
-        self.price_by_night = kwargs.get('price_by_night', 0)
-        self.latitude = kwargs.get('latitude', 0.0)
-        self.longitute = kwargs.get('longitude', 0.0)
-        self.amenity_ids = kwargs.get('amenity_ids', [])
+    __tablename__ = 'places'
+    city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable+True)
--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -14,3 +16,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
