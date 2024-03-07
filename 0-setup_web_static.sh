a9ced0293301406080b8c9556df7729a488ea739
Pibo Shongwe <piboman@yahoo.com>
2024-03-07 11:35:19 -0800

    u

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
-# Sets up web servers for the deployment of web_static folder
+# Sets up a web server for deployment of web_static.
 
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello World" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Grant permissions to user and group
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-printf %s "server {
-    listen 80;
+# Backup the original Nginx configuration
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+
+# Update the Nginx configuration
+sudo tee /etc/nginx/sites-available/default << 'EOF'
+server {
+    listen 80 default_server;
     listen [::]:80 default_server;
-    root   /etc/nginx/html;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
     index  index.html index.htm;
-    add_header X-Served-By \$hostname;
 
     location /hbnb_static {
-        alias /data/web_static/current/;
+        alias /data/web_static/current;
         index index.html index.htm;
     }
 
     location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        return 301 http://cuberule.com/;
     }
 
     error_page 404 /404.html;
     location /404 {
-    	root /etc/nginx/html;
-	internal;
+      root /var/www/html;
+      internal;
     }
 }
-" > /etc/nginx/sites-available/default
+EOF
 
-service nginx restart
+sudo service nginx restart
