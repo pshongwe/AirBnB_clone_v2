6296c4cb69c66beed2ca64753ca8798cfb374418
Pibo Shongwe <piboman@yahoo.com>
2024-03-24 07:40:40 -0700

    u

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application that displays a
+list of states from the database.
+"""
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of all states."""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
