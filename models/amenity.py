ddc1a74c274b332f1c70c4abbfc1e818d5e45dbc
Pibo Shongwe <piboman@yahoo.com>
2024-03-24 15:08:37 -0700

    u

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Table
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -10,6 +11,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-            "Place", secondary="place_amenity",
+            "Place", secondary=place_amenity,
             overlaps="amenities",
             viewonly=False)
