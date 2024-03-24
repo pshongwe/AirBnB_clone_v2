d04dc8def0512e6c55cda36fabdff87886e5dde5
Pibo Shongwe <piboman@yahoo.com>
2024-03-24 05:56:17 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """hello HBNB"""
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """HBNB func"""
+    """Display 'HBNB'"""
     return "HBNB"
 
 
